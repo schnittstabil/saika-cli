a7cc64751529ff4aeca6054c34229fd4963b1592
Michael Mayer <michael@schnittstabil.de>
2018-08-19 14:55:07 +0200

    workaround humbug/box#253

---
+++ build.php
@@ -0,0 +1,29 @@
+<?php
+
+# clean
+foreach (['saika.phar', 'saika_box.phar'] as $file) {
+    unlink($file);
+}
+
+
+# create saika.phar
+system("box compile");
+
+
+# repack
+rename('saika.phar', 'saika_box.phar');
+$dir = sys_get_temp_dir();
+$org = new Phar('saika_box.phar');
+$org->extractTo($dir);
+$repack = new Phar('saika.phar');
+$repack->buildFromDirectory($dir);
+$repack->setDefaultStub('saika');
+system("rm -rf $dir");
+
+
+# owner, group and permissions
+foreach (['saika.phar', 'saika_box.phar'] as $file) {
+    chown($file, 1000);
+    chgrp($file, 1000);
+    chmod($file, 0755);
+}
--- maintaining.md
+++ maintaining.md
@@ -3,11 +3,13 @@
 ## Build PHAR
 
 ```sh
+docker run --rm -it -v $PWD:/app dockerstabil/box php build.php
+
+
 docker run --rm -it -v $PWD:/app dockerstabil/box bash
 
 composer update --ignore-platform-reqs
 composer install --ignore-platform-reqs --no-dev
-box compile
-chown 1000:1000 saika.phar
+php build.php
 exit
 ```
